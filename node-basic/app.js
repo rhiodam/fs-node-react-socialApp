1a7a771ecf10af23e7ab596314442fcf12f5966f
Rhioda muthie <rhioda.m@gmail.com>
2019-04-06 18:11:45 +0700

    11 node core module

--- node-basic/app.js
+++ node-basic/app.js
@@ -4,11 +4,29 @@
 
 // Object Destructuring
 // extract some method or function from other module
-const  {sum , sum2} = require("./helpers");
+// const  {sum , sum2} = require("./helpers");
+
+// const total = sum(10,200);
+// const total2 = sum2(10,200);
+
+// console.log("TOTAL 1 : " , total);
+// console.log("TOTAL 2 : " , total2);
 
 
-const total = sum(10,200);
-const total2 = sum2(10,200);
+// this was from node core module
+const http = require('http');
 
-console.log("TOTAL 1 : " , total);
-console.log("TOTAL 2 : " , total2);
+
+const server = http.createServer((req,res)=>{
+    res.end("Hello world");
+    // console.log("request coming..." , req)
+})
+
+server.listen(3000);
+
+
+
+
+
+
+
