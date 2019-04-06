9f4bde872750a27c8d68c9b4c58912070f769430
Rhioda muthie <rhioda.m@gmail.com>
2019-04-07 02:08:27 +0700

    02 add separate routes

--- node-api/app.js
+++ node-api/app.js
@@ -1,6 +1,19 @@
+// Using node modules
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
+// Bringing routes
+// const postRoutes = require('./routes/post');
+const {getPosts} = require('./routes/post');
+
+
+// Middleware
+app.use()
+
+
+// app.get("/posts" , postRoutes.getPosts);
+app.get("/posts" , getPosts);
 
 app.get("/" , (req,res) =>{
     res.send("Hello world from express");
